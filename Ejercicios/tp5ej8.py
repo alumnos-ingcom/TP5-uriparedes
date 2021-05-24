70b1daf4aa106f6b9e1696fc1f594f1be7c3933b
uriparedes <urielchinoparedes@gmail.com>
2021-05-24 19:16:13 -0300

    Ejercicio 8 resuelto

--- Ejercicios/tp5ej8.py
+++ Ejercicios/tp5ej8.py
@@ -3,23 +3,60 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-def codificacion(texto):
+def rot13(texto):
     codificado = ""
-    for i in range(len(texto)):
-        codificado += (chr(ord(texto[i])+13))
+    for i in texto:
+        char = ord(i)
+        if(char >= ord("a") and char <= ord("z")):
+            if char > ord("m"):
+                char -= 13
+            else:
+                char += 13
+        elif(char >= ord("A") and char <= ord("Z")):
+            if char > ord("M"):
+                char -= 13
+            else:
+                char += 13
+        elif(char >= ord("0") and char <= ord ("9")):
+            if(char < ord("7")):
+                char += 3
+            else:
+                char -= 7
+        print(f"{i}({ord(i)}) -> {chr(char)}({char}))")
+        codificado += chr(char)
     return codificado
 
-def decodificacion(texto):
+def deRot13(texto):
     codificado = ""
-    for i in range(len(texto)):
-        codificado += (chr(ord(texto[i])-13))
+    for i in texto:
+        char = ord(i)
+        if(char >= ord("a") and char <= ord("z")):
+            if char > ord("m"):
+                char -= 13
+            else:
+                char += 13
+        elif(char >= ord("A") and char <= ord("Z")):
+            if char > ord("M"):
+                char -= 13
+            else:
+                char += 13
+        elif(char >= ord("0") and char <= ord ("9")):
+            if(char < ord("3")):
+                char += 7
+            else:
+                char -= 3
+        print(f"{i}({ord(i)}) -> {chr(char)}({char}))")
+        codificado += chr(char)
     return codificado
+
 
 def prueba():
-    texto = codificacion("hola")
+    texto = input("ingrese un texto a cifrar ")
+    texto = rot13(texto)
     print(texto)
-    texto = decodificacion(texto)
-    print(texto)
+    texto = deRot13(texto)
+    print (texto)
+
 
 if __name__ == "__main__":
     prueba()
